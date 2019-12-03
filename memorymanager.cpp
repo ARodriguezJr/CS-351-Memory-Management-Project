9d0023da079105bfa5d0cf86510c87d67ce17688
root <root@localhost.localdomain>
2019-12-03 02:19:22 -0500

    Base code works now

--- memorymanager.cpp
+++ memorymanager.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-int main(int argc, char* argv){
+int main(int argc, char** argv){
 	int memSize;
 	cout << "Memory Size> ";
 	cin >> memSize;
 
 	int pageSize;
-	cout << "\n\nPage Size (1: 100, 2: 200, 3: 400)> ";
+	cout << "\nPage Size (1: 100, 2: 200, 3: 400)> ";
 	cin >> pageSize;
 
 	return 0;
