2648d84e81ad287caca7c65e4e92b6e200aa0cd4
root <root@localhost.localdomain>
2019-12-03 02:15:30 -0500

    Initial code push

---
+++ memorymanager.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv){
+	int memSize;
+	cout << "Memory Size> ";
+	cin >> memSize;
+
+	int pageSize;
+	cout << "\n\nPage Size (1: 100, 2: 200, 3: 400)> ";
+	cin >> pageSize;
+
+	return 0;
+}
+
+
+
